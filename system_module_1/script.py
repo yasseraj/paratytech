5e1d2d3246a2117ec2edb49d94cb05f6c5480d1a
Yasser Aoujil <aoujil.yasser@gmail.com>
2021-12-09 12:33:18 +0100

    -Added query page using url param -Added datatable.js to show data

--- system_module_1/script.py
+++ system_module_1/script.py
@@ -1,9 +1,31 @@
+# imports
 import argparse
+import pandas as pd
+import requests
 
 # Set the argument parser and parse argument
 ap = argparse.ArgumentParser()
-ap.add_argument("-f", "--file", required=True, help="path to input file")
+ap.add_argument("-f", "--file", required=True, help="path to input file", default="resources/netflix_titles.csv")
+ap.add_argument("-e", "--endpoint", required=True, help="endpoint url", default="https://my-project-1487175171975.ew.r.appspot.com/titles")
 args = vars(ap.parse_args())
 # get input file path
 file_path = args["file"]
+# get endpoint url
+endpoint = args["endpoint"]
 
+
+def post_data(data):
+    """Posts data to the endpoint"""
+    response = requests.post(endpoint, json=data)
+    print(response.status_code)
+
+
+def read_file():
+    """Reads the file and sends every chunk to the specified endpoint"""
+    # Operate on chunks of 1000 entries to not overload memory.
+    for chunk in pd.read_csv(file_path, chunksize=500):
+        result = chunk.to_json(orient="records")
+        post_data(result)
+
+
+read_file()
--- system_module_2/main.py
+++ system_module_2/main.py
@@ -1,18 +1,49 @@
-import datetime
+import json
+from flask import Flask, render_template, request, Response
+from google.cloud import datastore
 
-from flask import Flask, render_template
-
+datastore_client = datastore.Client()
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    dummy_times = [datetime.datetime(2021, 12, 7, 10, 0, 0),
-                   datetime.datetime(2021, 12, 8, 10, 30, 0),
-                   datetime.datetime(2021, 12, 9, 11, 0, 0),
-                   ]
+@app.route('/', methods=['GET'])
+def queryData():  # put application's code here
+    """Retrieves data from Datastore using the limit url param"""
+    limit = request.args.get('limit', default=100, type=int)
+    titles = fetch_titles(limit)
+    return render_template('index.html', titles=titles)
 
-    return render_template('index.html', times=dummy_times)
+def fetch_titles(limit):
+    query = datastore_client.query(kind='Title')
+    titles = query.fetch(limit=limit)
+    return titles
+
+
+@app.route('/titles', methods=['POST'])
+def postDataHandler():
+    if not request.data:
+        # Can't send GET request to this endpoint
+        return Response(status=405)
+    if not request.is_json:
+        return Response(status=400)
+    # Parse posted data into list of dict
+    content = request.get_json()
+    data = json.loads(content)
+    # Store the data in Datastore
+    store_title(data)
+    # Return success status
+    status_code = Response(status=200)
+    return status_code
+
+def store_title(titles):
+    """Creates a batch job to put the data in google cloud datastore"""
+    batch = datastore_client.batch()
+    batch.begin()
+    for title in titles:
+        entity = datastore.Entity(key=datastore_client.key('Title'))
+        entity.update(title)
+        batch.put(entity)
+    batch.commit()
 
 
 if __name__ == '__main__':
